b64b79996a88bab26abed509ec0fd50a6330c7ad
Robert Ying <rbtying@aeturnalus.com>
2020-04-11 17:46:14 -0700

    Add frontend types for the structured messages

--- frontend/src/Chat.tsx
+++ frontend/src/Chat.tsx
@@ -1,7 +1,38 @@
 import * as React from 'react';
 import classNames from 'classnames';
+import {IGameMode} from './types';
 
-export type Message = {from: string; message: string; from_game?: boolean};
+export type MessageVariant =
+  | 'ResettingGame'
+  | 'StartingGame'
+  | {TrickWon: {winner: number; points: number}}
+  | {RankAdvanced: {player: number; new_rank: number}}
+  | {NewLandlordForNextGame: {landlord: number}}
+  | {PointsInKitty: {points: number; multiplier: number}}
+  | {JoinedGame: {player: number}}
+  | {JoinedTeam: {player: number}}
+  | {LeftGame: {name: string}}
+  | {KittySizeSet: {size: number | null}}
+  | {NumDecksSet: {num_decks: number | null}}
+  | {NumFriendsSet: {num_friends: number | null}}
+  | {GameModeSet: {game_mode: IGameMode}}
+  | 'TookBackPlay'
+  | {SetDefendingPointVisibility: {visible: boolean}}
+  | {SetLandlord: {landlord: number | null}}
+  | {SetRank: {rank: string}}
+  | {MadeBid: {card: string; count: number}};
+
+export type BroadcastMessage = {
+  actor: number;
+  variant: MessageVariant;
+};
+
+export type Message = {
+  from: string;
+  message: string;
+  from_game?: boolean;
+  data?: BroadcastMessage;
+};
 type Props = {
   messages: Message[];
 };
