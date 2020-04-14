f6d94e6abb04b52abee80ed10898e93ce18a5c46
Robert Ying <rbtying@aeturnalus.com>
2020-04-13 23:03:48 -0700

    Add visibility test for chat scroll

--- frontend/src/Chat.tsx
+++ frontend/src/Chat.tsx
@@ -21,7 +21,19 @@ const Chat = (props: Props) => {
       messages[messages.length - 1] !== latestMessage
     ) {
       latestMessage = messages[messages.length - 1];
-      anchor.current?.scrollIntoView({block: 'nearest', inline: 'start'});
+
+      if (anchor.current) {
+        const rect = anchor.current.getBoundingClientRect();
+        const html = document.documentElement;
+        const isVisible =
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || html.clientHeight) &&
+          rect.right <= (window.innerWidth || html.clientWidth);
+        if (isVisible) {
+          anchor.current?.scrollIntoView({block: 'nearest', inline: 'start'});
+        }
+      }
     }
   });
 
