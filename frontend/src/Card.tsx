8cfaf1331fb7e3febd50039427195dc8db0b9007
Robert Ying <rbtying@aeturnalus.com>
2020-07-03 16:02:42 -0700

    Use a canvas hack to constrain the bounding box of a card
    If you use a canvas element, apparently you can get the cards to be
    correctly bounded with comparatively limited hacks on the actual CSS
    side. This is kind of terrible, especially the bit where we make a
    temporary span just to grab a value out of the CSS file. But... it seems
    to work?
    
    tmp
    
    try to fix on Windows
    
    Handle device pixel ratio for hidpi computers
    
    try to line things up; set dpr to 1.5x the real dpr
    
    update offset a bit

--- frontend/src/Card.tsx
+++ frontend/src/Card.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import classNames from "classnames";
+import memoize from "./memoize";
 import InlineCard from "./InlineCard";
 import { cardLookup } from "./util/cardHelpers";
 import { SettingsContext } from "./AppStateProvider";
@@ -19,7 +20,11 @@ const Card = (props: IProps): JSX.Element => {
   if (!(props.card in cardLookup)) {
     const nonSVG = (
       <span className={classNames("card", "unknown", props.className)}>
-        {props.card}
+        <CardCanvas
+          card={props.card}
+          height={props.smaller ? 95 : 120}
+          suit={classNames("unknown", settings.fourColor ? "four-color" : null)}
+        />
       </span>
     );
 
@@ -50,7 +55,14 @@ const Card = (props: IProps): JSX.Element => {
         <div className="card-label">
           <InlineCard card={props.card} />
         </div>
-        {cardInfo.display_value}
+        <CardCanvas
+          card={cardInfo.display_value}
+          height={props.smaller ? 95 : 120}
+          suit={classNames(
+            cardInfo.typ,
+            settings.fourColor ? "four-color" : null
+          )}
+        />
       </span>
     );
 
@@ -76,6 +88,81 @@ const Card = (props: IProps): JSX.Element => {
       return nonSVG;
     }
   }
+};
+
+const computeCanvasBounds = (font: string): [number, number, number] => {
+  const c = document.createElement("canvas");
+  c.style.display = "none";
+  document.body.appendChild(c);
+  const ctx = c.getContext("2d");
+  ctx.font = font;
+  const text = "🂠";
+  const textMetrics = ctx.measureText(text);
+  const width =
+    textMetrics.actualBoundingBoxLeft + textMetrics.actualBoundingBoxRight;
+  const height =
+    textMetrics.actualBoundingBoxAscent + textMetrics.actualBoundingBoxDescent;
+  document.body.removeChild(c);
+  return [width, height, textMetrics.actualBoundingBoxDescent];
+};
+
+const computeSuitColor = (suit: string): string => {
+  const c = document.createElement("span");
+  c.className = suit;
+  c.style.display = "none";
+  document.body.appendChild(c);
+  const color = getComputedStyle(c).color;
+  document.body.removeChild(c);
+  return color;
+};
+
+const cardBoundsCache: { [font: string]: () => [number, number, number] } = {};
+const suitColorCache: { [suit: string]: () => string } = {};
+
+interface ICardCanvasProps {
+  card: string;
+  height: number;
+  suit: string;
+}
+
+const CardCanvas = (props: ICardCanvasProps): JSX.Element => {
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const dpr =
+    (window.devicePixelRatio !== undefined ? window.devicePixelRatio : 1) * 1.5;
+  const font = `${props.height * dpr}px solid`;
+  if (!(font in cardBoundsCache)) {
+    cardBoundsCache[font] = memoize(() => computeCanvasBounds(font));
+  }
+  if (!(props.suit in suitColorCache)) {
+    suitColorCache[props.suit] = memoize(() => computeSuitColor(props.suit));
+  }
+  const [width, height, offset] = cardBoundsCache[font]();
+  const style = suitColorCache[props.suit]();
+
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.font = font;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "bottom";
+    ctx.clearRect(0, 0, width + 2, height + 2);
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(2, 2, width, height);
+    ctx.fillStyle = style;
+    ctx.fillText(props.card, width / 2 + 1, height + offset - 3 * dpr);
+  });
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width={width + 2}
+      height={height + 2}
+      style={{
+        width: width / dpr + 2,
+        height: height / dpr + 2,
+      }}
+    />
+  );
 };
 
 export default Card;
