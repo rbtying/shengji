1c57724342e364de1919f33485c61b6540821b1c
Robert Ying <rbtying@aeturnalus.com>
2020-07-11 12:58:49 -0700

    Minor UI cleanups, and a generate-room button in JoinRoom (#183)
    * Minor UI cleanups, and a generate-room button in JoinRoom
    
    * prettier
    
    * more lint cleanup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,14 +8,6 @@ import TimerProvider from "./TimerProvider";
 import Root from "./Root";
 
 const bootstrap = (): void => {
-  if (window.location.hash.length !== 17) {
-    const arr = new Uint8Array(8);
-    window.crypto.getRandomValues(arr);
-    const r = Array.from(arr, (d) => ("0" + d.toString(16)).substr(-2)).join(
-      ""
-    );
-    window.location.hash = r;
-  }
   const root = document.getElementById("root");
   ReactModal.setAppElement(root);
   ReactDOM.render(
