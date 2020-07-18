b4e6c3e04adcfc1192a3f52b01dd73e042078c8c
Robert Ying <rbtying@aeturnalus.com>
2020-07-18 15:54:25 -0700

    lint fix

--- frontend/src/Initialize.tsx
+++ frontend/src/Initialize.tsx
@@ -343,7 +343,7 @@ const Initialize = (props: IProps): JSX.Element => {
     props.state.propagated.num_decks > 0
       ? props.state.propagated.num_decks
       : Math.floor(props.state.propagated.players.length / 2);
-  let kittyOffset =
+  const kittyOffset =
     (decksEffective * 54) % props.state.propagated.players.length;
 
   let currentPlayer = props.state.propagated.players.find(
