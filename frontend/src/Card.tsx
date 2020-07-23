388f4f27b7f5920a6e86729bc536c9858177e9e0
Robert Ying <rbtying@aeturnalus.com>
2020-07-23 10:50:37 -0700

    Tighten card boundaries using Canvas tricks (#192)
    * Use a canvas hack to constrain the bounding box of a card
    
    If you use a canvas element, apparently you can get the cards to be
    correctly bounded with comparatively limited hacks on the actual CSS
    side. This is kind of terrible, especially the bit where we make a
    temporary span just to grab a value out of the CSS file. But... it seems
    to work?
    
    * Use SVG for rendering rather than canvas to help deal with pixel-ratio issues

--- frontend/src/Card.tsx
+++ frontend/src/Card.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import classNames from "classnames";
+import memoize from "./memoize";
 import InlineCard from "./InlineCard";
 import { cardLookup } from "./util/cardHelpers";
 import { SettingsContext } from "./AppStateProvider";
@@ -19,7 +20,11 @@ const Card = (props: IProps): JSX.Element => {
   if (!(props.card in cardLookup)) {
     const nonSVG = (
       <span className={classNames("card", "unknown", props.className)}>
-        {props.card}
+        <CardCanvas
+          card={props.card}
+          height={props.smaller ? 95 : 120}
+          suit={classNames("unknown", settings.fourColor ? "four-color" : null)}
+        />
       </span>
     );
 
@@ -50,7 +55,14 @@ const Card = (props: IProps): JSX.Element => {
         <div className="card-label">
           <InlineCard card={props.card} />
         </div>
-        {cardInfo.display_value}
+        <CardCanvas
+          card={cardInfo.display_value}
+          height={props.smaller ? 95 : 120}
+          suit={classNames(
+            cardInfo.typ,
+            settings.fourColor ? "four-color" : null
+          )}
+        />
       </span>
     );
 
@@ -76,6 +88,87 @@ const Card = (props: IProps): JSX.Element => {
       return nonSVG;
     }
   }
+};
+
+const computeCanvasBounds = (font: string, dpr: number): TextMetrics => {
+  const c = document.createElement("canvas");
+  c.style.display = "none";
+  document.body.appendChild(c);
+  const ctx = c.getContext("2d");
+  ctx.scale(dpr, dpr);
+  ctx.font = font;
+  const text = "🂠";
+  const textMetrics = ctx.measureText(text);
+  document.body.removeChild(c);
+  return textMetrics;
+};
+
+const computeSuitColor = (suit: string): string => {
+  const c = document.createElement("span");
+  c.className = suit;
+  c.style.display = "none";
+  document.body.appendChild(c);
+  const color = getComputedStyle(c).color;
+  document.body.removeChild(c);
+  return color;
+};
+
+const cardBoundsCache: { [font: string]: () => TextMetrics } = {};
+const suitColorCache: { [suit: string]: () => string } = {};
+
+interface ICardCanvasProps {
+  card: string;
+  height: number;
+  suit: string;
+}
+
+const CardCanvas = (props: ICardCanvasProps): JSX.Element => {
+  const font = `${props.height}px solid`;
+  if (!(font in cardBoundsCache)) {
+    cardBoundsCache[font] = memoize(() => computeCanvasBounds(font, 1));
+  }
+  if (!(props.suit in suitColorCache)) {
+    suitColorCache[props.suit] = memoize(() => computeSuitColor(props.suit));
+  }
+  const textMetrics = cardBoundsCache[font]();
+  const style = suitColorCache[props.suit]();
+
+  const effectiveHeight = Math.round(
+    textMetrics.actualBoundingBoxAscent +
+      textMetrics.actualBoundingBoxDescent +
+      2
+  );
+  const effectiveWidth = Math.round(
+    textMetrics.actualBoundingBoxRight +
+      Math.min(textMetrics.actualBoundingBoxLeft, 0) +
+      2
+  );
+  return (
+    <svg
+      focusable="false"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      height={effectiveHeight}
+      width={effectiveWidth}
+    >
+      <rect
+        fill="#fff"
+        x={textMetrics.actualBoundingBoxLeft}
+        y={0}
+        width={textMetrics.width - 2}
+        height={effectiveHeight}
+      ></rect>
+      <text
+        fill={style}
+        fontSize={`${props.height}px`}
+        textLength={`${textMetrics.width}px`}
+        x={Math.min(textMetrics.actualBoundingBoxLeft, 0) + 1}
+        y={effectiveHeight - textMetrics.actualBoundingBoxDescent - 1}
+      >
+        {props.card}
+      </text>
+    </svg>
+  );
 };
 
 export default Card;
