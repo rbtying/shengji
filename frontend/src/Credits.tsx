cc5035cbfdca08d4fa57a761000a394d9b15d17c
David Kisluk <davidkisluk@pop-os.localdomain>
2021-01-18 15:38:39 -0500

    Update PointCardNotAllowed policy

--- frontend/src/Credits.tsx
+++ frontend/src/Credits.tsx
@@ -41,6 +41,13 @@ const ChangeLog = (): JSX.Element => {
         style={{ content: contentStyle }}
       >
         <h2>Change Log</h2>
+        <p>1/18/2021:</p>
+        <ul>
+          <li>
+            Ammend the "PointCardNotAllowed" friend selection policy. King is
+            now a valid friend when the landlord's rank is Ace.
+          </li>
+        </ul>
         <p>1/8/2021:</p>
         <ul>
           <li>
--- frontend/src/Initialize.tsx
+++ frontend/src/Initialize.tsx
@@ -51,7 +51,7 @@ const DifficultySettings = (props: IDifficultyProps): JSX.Element => {
               Non-trump cards, except the highest
             </option>
             <option value="PointCardNotAllowed">
-              Non-trump, non-point cards
+              Non-trump, non-point cards, unless the highest
             </option>
           </select>
         </label>
