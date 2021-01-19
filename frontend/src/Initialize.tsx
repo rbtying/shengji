042462453c0f51c53d749bf7e24088e0e371e57e
David Kisluk <dkgramming@users.noreply.github.com>
2021-01-18 19:04:59 -0500

    Update Initialize.tsx

--- frontend/src/Initialize.tsx
+++ frontend/src/Initialize.tsx
@@ -51,7 +51,7 @@ const DifficultySettings = (props: IDifficultyProps): JSX.Element => {
               Non-trump cards, except the highest
             </option>
             <option value="PointCardNotAllowed">
-              Non-trump, non-point cards, unless the highest
+              Non-trump, non-point cards (except K when playing A)
             </option>
           </select>
         </label>
