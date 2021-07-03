6b1fc02a3164c4870d30469a793f4b1378c6a3b7
Robert Ying <rbtying@aeturnalus.com>
2021-07-02 17:35:49 -0700

    Initial implementation of a redisstorage for basic key/value

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -1,5 +1,7 @@
 mod hash_map_storage;
+mod redis_storage;
 mod storage;
 
 pub use crate::hash_map_storage::HashMapStorage;
+pub use crate::redis_storage::{RedisStorage, RedisStorageError};
 pub use crate::storage::{State, Storage};
